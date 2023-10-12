cc9729bd6e262b76f438e64308527f33c0f0fdcf
Alby Chawula <albychawula@gmail.com>
2023-10-12 20:44:14 +0800

    Fix failing test_retrieve_all_records()

--- tests/database_test.py
+++ tests/database_test.py
@@ -43,7 +43,7 @@ class TestDatabase():
 
 
     def test_retrieve_all_records(self, test_moisture_data, test_db_path, setup_dummy_database):
-        records = database.get_all_moisture(test_db_path)
+        records = database.get_all_moisture_from_device(0, test_db_path)
 
         for record in records:
             print(record)
@@ -52,9 +52,16 @@ class TestDatabase():
         # Get each line from the test data file
         with open(test_moisture_data, 'r') as f:
             for line in f.readlines():
-                lines.append(line)
+                print(line.strip().split(','))
+                if line.strip().split(',')[3] == '0':
+                    lines.append(line)
 
-        assert len(lines) == len(records) + 1
+        # Get correct number of rows based on device id
+        assert len(lines) == len(records)
+
+    
+    def test_retrieve_single_records(self):
+        pass
 
     def test_insert(self):
         """Insert moisture table records."""
