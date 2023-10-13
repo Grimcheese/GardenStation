cea3a1dfadebc3ee30f75950e4e0cd1d15280049
Alby Chawula <albychawula@gmail.com>
2023-10-13 19:05:35 +0800

    Refactor database into Database class
    Using Database objects to manage the file path to use simplifies
    method calls using the database.

--- webserver/database.py
+++ webserver/database.py
@@ -4,6 +4,8 @@ import sqlite3
 import datetime
 from pathlib import Path
 
+
+
 DATABASE_FILENAME = "database.db"
 SCHEMA_FILENAME = "schema.sql"
 
@@ -11,106 +13,115 @@ root_dir = Path(__file__).parents[1]
 db_path = root_dir.joinpath("db", DATABASE_FILENAME)
 schema_path = root_dir.joinpath("db", SCHEMA_FILENAME)
 
-def open_standard_connection(database=db_path):
-    """Create a new connection to the specified database."""
-    
-    connection = sqlite3.connect(database)
-    return connection
+class Database:
+
+    def __init__(self, path=db_path, schema=schema_path):
+        self.database = path
+
+        self._create_from_schema(schema)
+
+    def open_standard_connection(self):
+        """Create a new connection to the specified database."""
+        
+        connection = sqlite3.connect(self.database)
+        return connection
 
 
-def open_row_connection(database=db_path):
-    """Open a connection for retrieving data."""
+    def open_row_connection(self):
+        """Open a connection for retrieving data."""
 
-    connection = sqlite3.connect(database)
-    connection.row_factory = sqlite3.Row
+        connection = sqlite3.connect(self.database)
+        connection.row_factory = sqlite3.Row
 
-    return connection
+        return connection
 
 
-def create_from_schema(database=db_path, sc_path=schema_path):
-    """Create a new database using the specified schema."""
-    
-    connection = open_standard_connection(database)
+    def _create_from_schema(self, schema=schema_path):
+        """Create a new database using the specified schema."""
+        
+        if db_path.exists():
+            db_path.unlink() # Ensure a new database is created
+        connection = self.open_standard_connection()
 
-    with open(sc_path) as f:
-        connection.executescript(f.read())
-    
-    connection.commit()
-    connection.close()
+        with open(schema) as f:
+            connection.executescript(f.read())
+        
+        connection.commit()
+        connection.close()
 
-def insert_record(table, database=db_path, **column_values):
-    connection = open_standard_connection(database)
-    cursor = connection.cursor()
+    def insert_record(self, table, **column_values):
+        connection = self.open_standard_connection()
+        cursor = connection.cursor()
 
-    # Get column names from table
-    """
-    cursor.execute("SELECT * FROM {table}")
-    columns = [description[0] for description in cursor.description]
-    
-    """
-    values = []
-    for key in column_values.keys():
-        values.append("?")
-    
-    # TODO Sanitise values in column_values
-    query = f"INSERT INTO {table} ({', '.join(column_values.keys())}) VALUES ({', '.join(values)})"
-    data = tuple(column_values.values())
+        # Get column names from table
+        """
+        cursor.execute("SELECT * FROM {table}")
+        columns = [description[0] for description in cursor.description]
+        
+        """
+        values = []
+        for key in column_values.keys():
+            values.append("?")
+        
+        # TODO Sanitise values in column_values
+        query = f"INSERT INTO {table} ({', '.join(column_values.keys())}) VALUES ({', '.join(values)})"
+        data = tuple(column_values.values())
 
-    cursor.execute(query, data)
-    connection.commit()
-    connection.close()
+        cursor.execute(query, data)
+        connection.commit()
+        connection.close()
 
-def insert_moisture_record(timestamp, reading, location, device_id, database=db_path):
-    """Create a new record in the moisture table.
-    
-    Args:
-        timestamp: String containing the date and time of record collection. 
-        reading: The moisture level recorded in the soil.
-        location: The name of the location where the reading was taken.
-        device_id: ID number of the device that took the reading.
-        database: Path to the database file. Default value is set by global
-            variable.
-    
-    """
-    connection = open_standard_connection(database)
-    
-    # TODO VALIDATE variables before using them in query
-    
-    cur = connection.cursor()
-    cur.execute("INSERT INTO moisture_readings (timestamp, moisture, location, device_id) VALUES (?, ?, ?, ?)",
-                    (timestamp, reading, location, device_id)
-                )
-    
-    connection.commit()
-    connection.close()
+    def insert_moisture_record(self, timestamp, reading, location, device_id):
+        """Create a new record in the moisture table.
+        
+        Args:
+            timestamp: String containing the date and time of record collection. 
+            reading: The moisture level recorded in the soil.
+            location: The name of the location where the reading was taken.
+            device_id: ID number of the device that took the reading.
+            database: Path to the database file. Default value is set by global
+                variable.
+        
+        """
+        connection = self.open_standard_connection()
+        
+        # TODO VALIDATE variables before using them in query
+        
+        cur = connection.cursor()
+        cur.execute("INSERT INTO moisture_readings (timestamp, moisture, location, device_id) VALUES (?, ?, ?, ?)",
+                        (timestamp, reading, location, device_id)
+                    )
+        
+        connection.commit()
+        connection.close()
 
-def get_all_moisture_from_device(device_id, database=db_path):
-    """Retrieve all moisture records from the specified database."""
-    
-    connection = open_row_connection()
-    cursor = connection.cursor()
+    def get_all_moisture_from_device(self, device_id):
+        """Retrieve all moisture records from the specified database."""
+        
+        connection = self.open_row_connection()
+        cursor = connection.cursor()
 
-    cursor.execute(f"SELECT * FROM moisture WHERE device_id = {device_id}")
-    
-    # Convert rows to dictionary key:value pairs
-    results = [dict(row) for row in cursor.fetchall()]
-    connection.close()
+        cursor.execute(f"SELECT * FROM moisture_readings WHERE device_id = {device_id}")
+        
+        # Convert rows to dictionary key:value pairs
+        results = [dict(row) for row in cursor.fetchall()]
+        connection.close()
 
-    return results
+        return results
 
-def get_moisture_from_device_range(device, start, end, database=db_path):
-    """Get all moisture readings from a device within a datetime range."""
-    
-    connection = open_row_connection()
-    cursor = connection.cursor()
+    def get_moisture_from_device_range(self, device, start, end):
+        """Get all moisture readings from a device within a datetime range."""
+        
+        connection = self.open_row_connection()
+        cursor = connection.cursor()
 
-    query = f"SELECT * FROM moisture WHERE device_id IS {device} \
-    AND timestamp >= start AND timestamp <= end"
-    
-    cursor.execute(query)
+        query = f"SELECT * FROM moisture WHERE device_id IS {device} \
+        AND timestamp >= {start} AND timestamp <= {end}"
+        
+        cursor.execute(query)
 
-    results = [dict(row) for row in cursor.fetchall()]
-    connection.close()
-    
-    return results
+        results = [dict(row) for row in cursor.fetchall()]
+        connection.close()
+        
+        return results
 
--- webserver/init_db.py
+++ webserver/init_db.py
@@ -2,17 +2,16 @@
 
 from pathlib import Path
 
-from . import database
+from .database import Database
 
 DATABASE_FILENAME = None
-SCHEMA_FILENAME = database.SCHEMA_FILENAME
+#SCHEMA_FILENAME = database.SCHEMA_FILENAME
 
 root_dir = Path(__file__).parents[1]
 
-def insert_moisture_test_data(test_data_file, db=None):
+def insert_moisture_test_data(db, test_data_file):
     """Insert moisture data from a csv file into specified database."""
 
-    test_moisture_path = Path.joinpath(root_dir, "misc", "src", "data", "test_moisture.txt")
     with open(test_data_file, 'r') as f:
         lines = f.readlines()
 
@@ -21,11 +20,7 @@ def insert_moisture_test_data(test_data_file, db=None):
             separated = line.strip().split(",")
             print(separated)
 
-            if db != None:
-                database.insert_moisture_record(f"{separated[0]}", separated[1], f"{separated[2]}", separated[3], db)
-            else:
-                database.insert_moisture_record(f"{separated[0]}", separated[1], f"{separated[2]}", separated[3])
-
+            db.insert_moisture_record(f"{separated[0]}", separated[1], f"{separated[2]}", separated[3])
 
 def setup_script(db_name=None):
     global db_path
@@ -41,18 +36,17 @@ def execute(moisture_test_data=None):
     print(f"Root directory used: {root_dir}")
     print(f"Database file being used: {db_path}")
 
+
+
     # Initialise a new database
     if db_path != None:
-        database.create_from_schema(database=db_path)
+        db = Database(path=db_path)
     else:
-        database.create_from_schema()
+        db = Database()
 
     if moisture_test_data != None:
         # Put data into new database
-        if db_path != None:
-            insert_moisture_test_data(moisture_test_data, db_path)
-        else:
-            insert_moisture_test_data(moisture_test_data)
+        insert_moisture_test_data(db, moisture_test_data)    
 
 
 def run_script(db_name=None, moisture_data=None):
