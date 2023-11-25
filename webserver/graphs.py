e1b84257636149cc88850734d959a1dbd02cfe29
Alby Chawula <albychawula@gmail.com>
2023-11-25 20:05:01 +0800

    Cleaniup, remove deprecated functions, add docs

--- webserver/graphs.py
+++ webserver/graphs.py
@@ -11,12 +11,18 @@ import plotly
 import plotly.graph_objects as go
 import plotly.express as px
 
-def get_default_data(db, device, start, end):
-    data = db.get_moisture_from_device_range(device, start, end)
-
-    return data
 
 def get_line_graph_JSON(data, xVal, yVal):
+    """Get the JSON representation of a plotly line graph.
+    
+    Args:
+        data: The data retrieved from the database to display in the graph.
+        xVal: Dictionary key to use for data displayed on the x-axis.
+        yVal: Dictionary key to use for data displayed on the y-axis.
+        
+    Return: A plotly graph encoded as a JSON object.
+    """
+    
     fig = px.line(data, x=xVal, y=yVal)
     
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
@@ -24,6 +30,14 @@ def get_line_graph_JSON(data, xVal, yVal):
 
 
 def generate_all_default_graphs(db):
+    """Using the database referenced with db - generate default graphs for website.
+    
+    Args:
+        db: Database object that contains soil moisture data to be displayed.
+        
+    Return: Dictionary of plotly graphs encoded into JSON objects. The key value
+        is the device_id for the device the data has been recorded from.
+    """
     # Get all device ids stored in database
     ids = db.get_all_ids()
     
@@ -41,7 +55,15 @@ def generate_all_default_graphs(db):
     
     return dsJSON
 
+
 def generate_moisture_graph(data):
+    """Create a plotly graph using data passed in as an argument.
+    
+    Args:
+        data: List of records retrieved from the SQLite database to be displayed. 
+            Each record must have a timestamp and moisture column to be accessed.
+    """
+    
     print("Data retrieved:")
     for element in data:
         print(element)
@@ -53,6 +75,7 @@ def generate_moisture_graph(data):
 
     fig = px.line(data, x="timestamp", y="moisture")
     fig.show()
+
 
 if __name__ == '__main__':
     db_path = Path(__file__).parents[1].joinpath("db","test_database.db")
