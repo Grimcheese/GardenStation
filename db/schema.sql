89decc133e35fb378e93103e141e6e3780e4a1a0
Alby Chawula <albychawula@gmail.com>
2024-04-27 19:25:16 +0800

    Updated schema script to follow database redesign
    Database now includes the following tables:
        soil_readings
        devices
        locations
        device_locations

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,37 @@
-DROP TABLE IF EXISTS moisture_readings;
-DROP TABLE IF EXISTS sensors;
+-- MySQL Schema script to create a new database according to
+-- schema diagram in Database documentation
 
-CREATE TABLE moisture_readings (
-    reading_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    timestamp TIMESTAMP NOT NULL,
-    moisture REAL NOT NULL,
-    location TEXT NOT NULL,
-    device_id INT NOT NULL
+DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS devices;
+DROP TABLE IF EXISTS soil_readings;
+DROP TABLE IF EXISTS device_locations;
+
+
+CREATE TABLE devices (
+    device_id           INTEGER PRIMARY KEY
+    software_version    VARCHAR(10) NOT NULL
+    microprocessor      VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE sensors (
-    device_id INTEGER PRIMARY KEY,
-    software_version TEXT
+CREATE TABLE locations (
+    location_id         INTEGER AUTOINCREMENT,
+    latitude            REAL NOT NULL
+    longitude           REAL NOT NULL
+    loc_address         VARCHAR(50)             
+);
+
+CREATE TABLE soil_readings (
+    reading_id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    reading_time        DATETIME NOT NULL,
+    soil_reading        REAL NOT NULL,
+    device_id           REFERENCES devices (device_id) NOT NULL,
+    location_id         REFERENCES locations (location_id) NOT NULL
+);
+
+CREATE TABLE device_locations (
+    device_id           REFERENCES devices (device_id),
+    location_id         REFERENCES locations (location_id),
+    PRIMARY KEY (device_id, location_id),
+    date_placed         DATE, NOT NULL
+    date_removed        DATE
 );
